5e4ba76c953424b12558d9763419fc2ca6ce48a4
DamZav <damaris.zavala91@gmail.com>
2025-01-27 18:10:54 -0600

    cambio related names en Usuario model

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,18 @@ class Usuario(AbstractUser):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=False) # Activación tras el pago
 
+    #  Solución a que chocaba: Cambiar el related_name de las relaciones 
+    # inversas
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Cambia el nombre inverso
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Cambia el nombre inverso
+        blank=True
+    )
 
 # Autenticación social: Integra django-allauth para login con Google/Facebook. 
 # Instala el paquete:
